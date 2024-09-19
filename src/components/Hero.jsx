b6b2f8b594803e9296ca2e5800badd33ce572260
Coste Ionut Petru <ionut4046@yahoo.com>
2024-09-19 17:48:21 +0100

    img1 added to hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,16 @@
+import profileImg1 from "../assets/profile1.jpeg";
+
 const Hero = () => {
   return (
     <div>
-        
+        <section className="flex min-h-screen flex-wrap items-center 
+        bg-emerald-950 text-orange-50">
+            <div className="w-full p-8 md:w-1/2">
+                <div className="mb-4 flex space-x-2">
+                   <img src={profileImg1} alt="" />
+                </div>
+            </div>
+        </section>
     </div>
   )
 }
