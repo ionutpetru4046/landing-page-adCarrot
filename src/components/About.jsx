0d45c59d8fed2de573884a28801a2e8242b6fe4d
Coste Ionut Petru <ionut4046@yahoo.com>
2024-09-19 19:29:17 +0100

    looped through images to put in the about section from constants

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,16 @@
+import { ABOUT } from "../constants"
+
 const About = () => {
   return (
     <section className="flex min-h-screen flex-col items-center justify-center 
     bg-emerald-50 px-6" id="about">
       <h2 className="my-8 text-center text-4xl font-semibold tracking-tighter">About Us</h2>
+      <div className="mb-10 flex items-center justify-center gap-4">
+        {ABOUT.profileImages.map((image, index) => (
+            <img key={index} src={image.src} alt={image.alt} className="w-1/4 
+            rounded-3xl shadow-lg" />
+        ))}
+      </div>
     </section>
   )
 }
