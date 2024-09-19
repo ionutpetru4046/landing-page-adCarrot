9e657c2e45fc43791fac9bb7ffe6ac5c53d7cc86
Coste Ionut Petru <ionut4046@yahoo.com>
2024-09-19 17:52:51 +0100

    second image added to hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,5 @@
 import profileImg1 from "../assets/profile1.jpeg";
+import profileImg2 from "../assets/profile2.jpeg";
 
 const Hero = () => {
   return (
@@ -7,7 +8,9 @@ const Hero = () => {
         bg-emerald-950 text-orange-50">
             <div className="w-full p-8 md:w-1/2">
                 <div className="mb-4 flex space-x-2">
-                   <img src={profileImg1} alt="" />
+                   <img src={profileImg1} alt="Team Member 1" className="h-12 w-12 rounded-full 
+                   border-2 border-orange-50" />
+                   <img src={profileImg2} alt="Team Member 2" />
                 </div>
             </div>
         </section>
