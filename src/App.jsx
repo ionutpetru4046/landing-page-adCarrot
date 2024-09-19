2a1b377810a8adfafa0f012cb87264e27eb762ed
Coste Ionut Petru <ionut4046@yahoo.com>
2024-09-19 22:35:51 +0100

    added services titles and looped through constants to get titles into the component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import About from "./components/About"
 import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
+import Services from "./components/Services"
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
      <Navbar />
      <Hero />
      <About />
+     <Services />
     </div>
   )
 }
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,9 +1,26 @@
+import { useState } from "react"
+import { SERVICES } from "../constants"
+
 const Services = () => {
+
+    const [activeTab, setActiveTab] = useState(SERVICES[0]);
+
   return (
     <section className="bg-stone-50 p-6 text-emerald-950" id="services">
         <div className="container mx-auto">
             <h2 className="my-8 text-center text-4xl font-semibold 
             tracking-lighter">Services</h2>
+            <div className="flex items-center justify-center space-x-4">
+                {SERVICES.map((tab) => (
+                    <button key={tab.id} onClick={() => setActiveTab(tab)} 
+                    className={`px-4 py-2 font-semibold tracking-tighter transition-colors 
+                    lg:text-2xl ${
+                        activeTab.id === tab.id ? "border-b-2 border-orange-500 text-orange-500" : "text-emerald-950"
+                    }`}>
+                        {tab.title}
+                    </button>
+                ))}
+            </div>
         </div>
     </section>
   )
