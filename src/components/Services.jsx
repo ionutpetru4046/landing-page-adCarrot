18a8c749327998f0cc693b860322507630435aa2
Coste Ionut Petru <ionut4046@yahoo.com>
2024-09-19 22:13:47 +0100

    changes

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -2,7 +2,8 @@ const Services = () => {
   return (
     <section className="bg-stone-50 p-6 text-emerald-950" id="services">
         <div className="container mx-auto">
-            <h2 className="my-8 text-center text-4xl font-semibold tracking-lighter">Services</h2>
+            <h2 className="my-8 text-center text-4xl font-semibold 
+            tracking-lighter">Services</h2>
         </div>
     </section>
   )
