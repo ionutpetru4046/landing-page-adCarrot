c6c7d9a2904c91c783f68bbcd59bd99e3c39bead
Coste Ionut Petru <ionut4046@yahoo.com>
2024-09-19 18:55:10 +0100

    added about us section with section and h2

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,9 @@
-
-
 const About = () => {
   return (
-    <div>
-      
-    </div>
+    <section className="flex min-h-screen flex-col items-center justify-center 
+    bg-emerald-50 px-6" id="about">
+      <h2 className="my-8 text-center text-4xl font-semibold tracking-tighter">About Us</h2>
+    </section>
   )
 }
 
