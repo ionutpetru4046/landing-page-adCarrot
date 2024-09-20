cdb876b0df3166318d51ecf8137852914cb38544
Coste Ionut Petru <ionut4046@yahoo.com>
2024-09-20 14:22:49 +0100

    h1 added to the new section

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,7 +1,11 @@
 const Testimonials = () => {
   return (
-    <section>
-        
+    <section className="bg-stone-50 px-8 py-20" id="reviews">
+        <h1 className="my-8 text-center text-4xl font-semibold 
+        tracking-tighter">What Clients Say</h1>
+        <div className="">
+
+        </div>
     </section>
   )
 }
