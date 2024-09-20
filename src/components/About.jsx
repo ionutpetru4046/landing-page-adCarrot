c101578f44a0e9fdfafd52e717a7026e029d4ae7
Coste Ionut Petru <ionut4046@yahoo.com>
2024-09-20 21:56:59 +0100

    motion animations added to about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,13 +1,27 @@
 import { ABOUT } from "../constants"
+import { motion } from "framer-motion"
 
 const About = () => {
+    const imageVariants = {
+      hidden: { opacity: 0, y: 50 },
+      visible: { opacity: 1, y: 0 }
+    }
   return (
     <section className="flex min-h-screen flex-col items-center justify-center 
     bg-emerald-50 px-6" id="about">
       <h2 className="my-8 text-center text-4xl font-semibold tracking-tighter">About Us</h2>
       <div className="mb-10 flex items-center justify-center gap-4">
         {ABOUT.profileImages.map((image, index) => (
-            <img key={index} src={image.src} alt={image.alt} className="w-1/4 
+            <motion.img 
+            variants={imageVariants}
+            initial= "hidden"
+            whileInView="visible"
+            viewport={{ once: true }}
+            transition={{ duration: 0.5, delay: image.delay }}
+            key={index} 
+            src={image.src} 
+            alt={image.alt} 
+            className="w-1/4 
             rounded-3xl shadow-lg" />
         ))}
       </div>
