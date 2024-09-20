44e79ca898766434c72a24d759f97beaad5c3553
Coste Ionut Petru <ionut4046@yahoo.com>
2024-09-20 21:07:56 +0100

    added the Fa inside the footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,30 @@
+import { FaFacebookF, FaInstagram, FaLinkedin } from "react-icons/fa"
+import { FaXTwitter } from "react-icons/fa6"
+
 const Footer = () => {
   return (
-    <section>
-        
-    </section>
+    <footer className="bg-emerald-950 py-6 text-orange-50">
+        <div className="container mx-auto flex flex-col items-center justify-center 
+        space-y-4 md:space-y-0">
+            <div className="mb-4 flex space-x-3">
+                <a href="#">
+                    <FaFacebookF size={20} />
+                </a>
+                <a href="#">
+                    <FaXTwitter size={20} />
+                </a>
+                <a href="#">
+                    <FaInstagram size={20} />
+                </a>
+                <a href="#">
+                    <FaLinkedin size={20} />
+                </a>
+            </div>
+            <div className="text-sm opacity-50">
+                {new Date().getFullYear()} AdCarrot. All Rights Reserved
+            </div>
+        </div>
+    </footer>
   )
 }
 
