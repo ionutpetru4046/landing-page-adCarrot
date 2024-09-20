fd57a8fbb5e090fcd7a19457b85ebb7893accd95
Coste Ionut Petru <ionut4046@yahoo.com>
2024-09-20 12:34:27 +0100

    Pricing title added to the new section

--- src/components/AdAgencyPackages.jsx
+++ src/components/AdAgencyPackages.jsx
@@ -1,7 +1,11 @@
 const AdAgencyPackages = () => {
   return (
-    <section className="">
-      
+    <section className="bg-stone-50 px-4 py-12 md:px-8" id="pricing">
+        <div>
+            <h2 className="my-8 text-center text-4xl font-semibold tracking-tighter">
+                Pricing
+            </h2>
+        </div>
     </section>
   )
 }
