a9b45fad0180ebbce564a5b0c5c9e8f1da9c973e
Coste Ionut Petru <ionut4046@yahoo.com>
2024-09-20 21:41:29 +0100

    added motion to the hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,6 +3,7 @@ import profileImg1 from "../assets/profile1.jpeg";
 import profileImg2 from "../assets/profile2.jpeg";
 import profileImg3 from "../assets/profile3.jpg";
 import heroImg from "../assets/hero-image.png"
+import { motion } from "framer-motion";
 
 const Hero = () => {
   return (
@@ -10,25 +11,60 @@ const Hero = () => {
         bg-emerald-950 text-orange-50">
         <div className="w-full p-8 md:w-1/2">
             <div className="mb-4 flex space-x-2">
-                   <img src={profileImg1} alt="Team Member 1" className="h-12 w-12 rounded-full 
+                   <motion.img 
+                   initial={{ opacity: 0, scale: 0.8 }}
+                   animate={{ opacity: 1, scale: 1 }}
+                   transition={{duration: 0.8, delay: 0.1}}
+                   src={profileImg1} 
+                   alt="Team Member 1" 
+                   className="h-12 w-12 rounded-full 
                    border-2 border-orange-50" />
-                   <img src={profileImg2} alt="Team Member 2" className="h-12 w-12 rounded-full 
+
+                   <motion.img 
+                   initial={{ opacity: 0, scale: 0.8 }}
+                   animate={{ opacity: 1, scale: 1 }}
+                   transition={{duration: 0.8, delay: 0.2}}
+                   src={profileImg2} 
+                   alt="Team Member 2" 
+                   className="h-12 w-12 rounded-full 
                    border-2 border-orange-50" />
-                   <img src={profileImg3} alt="Team Member 3" className="h-12 w-12 rounded-full 
+
+                   <motion.img 
+                   initial={{ opacity: 0, scale: 0.8 }}
+                   animate={{ opacity: 1, scale: 1 }}
+                   transition={{duration: 0.8, delay: 0.3}}
+                   src={profileImg3} 
+                   alt="Team Member 3" 
+                   className="h-12 w-12 rounded-full 
                    border-2 border-orange-50" />
+
                 </div>
-                <h1 className="text-4xl tracking-tighter md:text-x-5xl lg:text-7xl">
+                <motion.h1 
+                initial={{ opacity: 0, y: -20 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.8 }}
+                className="text-4xl tracking-tighter md:text-x-5xl lg:text-7xl">
                     We create impactful ads that
                     <span className="text-orange-500"> elevate</span> your brand.
-                </h1>
-            <button className="mt-8 flex items-center gap-2 rounded-full bg-emerald-900 px-4 py-3 
+                </motion.h1>
+            <motion.button 
+            initial={{ opacity: 0, y: -20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1 }}
+            className="mt-8 flex items-center gap-2 rounded-full bg-emerald-900 px-4 py-3 
                 hover:bg-emerald-800">
                 <span>Know More</span>
                 <IoArrowForward />
-            </button>
+            </motion.button>
         </div>
         <div className="w-full md:w-1/2 lg:p-8 ">
-            <img src={heroImg} alt="Ad Agency" className="h-auto w-full" />
+            <motion.img 
+            initial={{ opacity: 0, scale: 0.8 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.8 }}
+            src={heroImg} 
+            alt="Ad Agency" 
+            className="h-auto w-full" />
         </div>
     </section>
   )
