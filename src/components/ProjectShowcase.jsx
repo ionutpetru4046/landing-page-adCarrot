94080bc9524aca402baab0b59a5bd19113783c6f
Coste Ionut Petru <ionut4046@yahoo.com>
2024-09-20 12:02:36 +0100

    images added in project section

--- src/components/ProjectShowcase.jsx
+++ src/components/ProjectShowcase.jsx
@@ -1,3 +1,5 @@
+import { PROJECTS } from "../constants"
+
 const ProjectShowcase = () => {
   return (
     <section className="container max-auto px-4 py-12 md:px-8" id="projects">
@@ -6,6 +8,16 @@ const ProjectShowcase = () => {
           font-semibold tracking-tighter">
             Projects
           </h2>
+        </div>
+        <div className="space-y-16">
+          {PROJECTS.map((project, index) => (
+            <div key={index} className="flex flex-col items-center md:flex-row-reverse md:items-start">
+              <div className="w-full p-8 md:w-1/2">
+                <img src={project.imageUrl} alt={project.title} 
+                className="w-full rounded-3xl" />
+              </div>
+            </div>
+          ))}
         </div>
     </section>
   )
