c3862cb3e7ac0c332021082e224c7923b81de153
Coste Ionut Petru <ionut4046@yahoo.com>
2024-09-20 12:23:14 +0100

    table added to projects section

--- src/components/ProjectShowcase.jsx
+++ src/components/ProjectShowcase.jsx
@@ -16,6 +16,34 @@ const ProjectShowcase = () => {
                 <img src={project.imageUrl} alt={project.title} 
                 className="w-full rounded-3xl" />
               </div>
+              <div className="w-full md:w-1/2">
+                <h2 className="mb-2 text-lg text-emerald-950">{project.year}    
+                </h2>
+                  <h3 className="mb-2 text-2xl font-semibold text-emerald-950">
+                     {project.title}
+                  </h3>
+                  <p className="mb-2 text-3xl text-emerald-950 lg:text-4xl">{project.description}</p>
+                  <p className="mb-4 text-lg text-emerald-950 lg:text-2xl">{project.details}</p>
+                  <table className="min-w-full text-lg lg:text-xl">
+                    <tbody>
+                      <tr>
+                        <td className="px-6 py-4 font-medium text-emerald-950">Investment</td>
+                        <td className="px-6 py-4 text-emerald-950">
+                          {project.figures.investment}</td>
+                      </tr>
+                      <tr>
+                        <td className="px-6 py-4 font-medium text-emerald-950">Duration</td>
+                        <td className="px-6 py-4 text-emerald-950">
+                          {project.figures.duration}</td>
+                      </tr>
+                      <tr>
+                      <td className="px-6 py-4 font-medium text-emerald-950">Outcome</td>
+                        <td className="px-6 py-4 text-emerald-950">
+                          {project.figures.outcome}</td>
+                      </tr>
+                    </tbody>
+                  </table>
+              </div>
             </div>
           ))}
         </div>
