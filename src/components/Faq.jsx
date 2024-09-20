d3386d72d8161d825f6fb10643bc9097cc84cc5b
Coste Ionut Petru <ionut4046@yahoo.com>
2024-09-20 15:29:41 +0100

    Faq section finished added some functions and styling for this component

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -1,4 +1,7 @@
 import { useState } from "react"
+import { FAQ_DESCRIPTION, FAQS } from "../constants";
+import { FaMinus, FaPlus } from "react-icons/fa";
+
 
 const Faq = () => {
 
@@ -9,8 +12,42 @@ const Faq = () => {
     }
 
   return (
-    <section className="">
-        
+    <section className="mt-20" id="faq">
+        <div className="container mx-auto flex flex-col gap-12 p-8 lg:flex-row">
+            <div className="lg:w-1/3">
+                <h2 className="mb-8 text-4xl font-semibold tracking-tighter">
+                    Frequently asked questions
+                </h2>
+                <p className="mb-12 text-lg">
+                    {FAQ_DESCRIPTION}
+                </p>
+            </div>
+            <div className="w-2/3">
+                {FAQS.map((faq, index) => (
+                    <div key={index} className="mb-4 border-b pb-4">
+                        <div className="flex cursor-pointer items-center 
+                        justify-between p-8" onClick={() =>handleToggle(index)}>
+                            <h3 className="text-2xl">{faq.question}</h3>
+                            <div>
+                                {activeIndex === index ? (
+                                    <FaMinus className="text-xl" />
+                                ) : (
+                                   <FaPlus className="text-xl" /> 
+                                )}
+                            </div>
+                        </div>
+                        <div className={`mt-4 overflow-hidden text-lg transition-all 
+                            duration-500 ${
+                                activeIndex === index ? "max-h-screen" : "max-h-0"
+                            }`}>
+                                <p className="p-8">
+                                    {faq.answer}
+                                </p>
+                            </div>
+                    </div>
+                ))}
+            </div>
+        </div>
     </section>
   )
 }
