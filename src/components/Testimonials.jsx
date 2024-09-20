b927f1ee380e1699e4571b026991702d83316ac9
Coste Ionut Petru <ionut4046@yahoo.com>
2024-09-20 14:36:06 +0100

    images added to the testimonial section looped through constants and styled

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,10 +1,19 @@
+import { TESTIMONIALS } from "../constants"
+
 const Testimonials = () => {
   return (
     <section className="bg-stone-50 px-8 py-20" id="reviews">
         <h1 className="my-8 text-center text-4xl font-semibold 
         tracking-tighter">What Clients Say</h1>
-        <div className="">
-
+        <div className="container mx-auto flex flex-wrap gap-8">
+            {TESTIMONIALS.map((testimonial, index) => (
+                <div key={index} className={`rounded-3xl p-4 ${testimonial.bgColor} ${testimonial.textColor} 
+                flex min-w-[250px] flex-1 flex-col items-start`}>
+                    <img src={testimonial.image} alt={testimonial.author} 
+                    className="mb-4 rounded-3xl object-cover" />
+                    <p className="mb-4 text-lg md:text-xl lg:text-2xl">{testimonial.text}</p>
+                </div>
+            ))}
         </div>
     </section>
   )
