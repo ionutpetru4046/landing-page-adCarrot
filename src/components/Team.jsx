908c8a8832d74fa23600f23416b1170c776a6ef6
Coste Ionut Petru <ionut4046@yahoo.com>
2024-09-20 14:01:18 +0100

    images team members added to the section and looped through

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -1,7 +1,17 @@
+import { TEAM_MEMBERS } from "../constants"
+
 const Team = () => {
   return (
     <section className="container mx-auto px-8 py-20" id="team">
         <h2 className="my-8 text-center text-4xl font-semibold tracking-tighter">Team</h2>
+        <div className="flex flex-wrap">
+            {TEAM_MEMBERS.map((member, index) => (
+                <div key={index} className="mb-12 flex w-full flex-col 
+                items-center px-4 text-center md:w-1/2 lg:w-1/3">
+                    <img src={member.image} alt="" />
+                </div>
+            ))}
+        </div>
     </section>
   )
 }
