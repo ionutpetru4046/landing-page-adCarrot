2cb0a07d748d87274cb88ba9d0ced22f5e4cdbed
Coste Ionut Petru <ionut4046@yahoo.com>
2024-09-20 13:22:34 +0100

    button added to the pricing section and looped through packages

--- src/components/AdAgencyPackages.jsx
+++ src/components/AdAgencyPackages.jsx
@@ -1,3 +1,4 @@
+import { IoArrowForward } from "react-icons/io5"
 import { PACKAGES } from "../constants"
 
 const AdAgencyPackages = () => {
@@ -17,7 +18,24 @@ const AdAgencyPackages = () => {
                         <div className="mb-6 flex flex-col md:mb-0 md:w-1/2">
                             <h2 className="mb-2 text-lg font-semibold 
                             tracking-tighter">{pkg.name}</h2>
+                            <p className="mb-2 text-5xl font-bold">{pkg.price}</p>
+                            <p className="px-2">{pkg.description}</p>
                         </div>
+                        <div className="flex flex-col md:w-1/2">
+                            <h3 className="mb-2 font-semibold tracking-tighter">What's included?</h3>
+                            <ul className="mb-4">
+                                {pkg.services.map((service, index) => (
+                                    <li key={index}>- {service}</li>
+                                ))}
+                            </ul>
+                            <div>
+                                <button className="flex items-center gap-2 
+                                rounded-full bg-emerald-950 px-4 py-2 text-orange-50 hover:bg-emerald-900">
+                                    <span>Get Started</span>
+                                    <IoArrowForward />
+                                </button>
+                            </div>
+                        </div>  
                     </div>
                 </div>
             ))}
